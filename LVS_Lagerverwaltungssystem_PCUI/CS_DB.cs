9caf6680018496fe5d62f0a8701e7430f50f6546
Chaftalie <chaftalie@Lapritrop>
2019-01-23 09:45:10 +0100

    Added SQL Stuff to DB stuff

--- LVS_Lagerverwaltungssystem_PCUI/CS_DB.cs
+++ LVS_Lagerverwaltungssystem_PCUI/CS_DB.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Odbc;
 using System.Linq;
 using System.Text;
@@ -20,5 +21,51 @@ namespace LVS_Lagerverwaltungssystem_PCUI
         private static string Database_Login_Password => "root";
 
         private static string Connectionstring => $"Driver={"MySQL ODBC 5.3 Unicode Driver"};Server={Database_IP};Port={Database_Port};Database={Database_Name};User={Database_Login_Name};Password={Database_Login_Password};Option=3;";
+    }
+
+    class SQL_methods
+    {
+
+        static private OdbcConnection Connection => DB.Connection;
+
+        /// <summary>
+        /// Füllt DataTables mit der gewünschten SQL-Query
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        static public DataTable Fill_Box(string sql)
+        {
+
+            var da = new OdbcDataAdapter(sql, Connection);
+            var dt_out = new DataTable();
+            da.Fill(dt_out);
+
+            return dt_out;
+        }
+
+        /// <summary>
+        /// Führt den SQL-Befehl aus. Es wird kein Con.Open benötigt!
+        /// </summary>
+        /// <param name="sql"></param>
+        static public void SQL_exec(string sql)
+        {
+            Open();
+            var cmd = new OdbcCommand(sql, Connection);
+            cmd.ExecuteNonQuery();
+
+
+        }
+
+
+        /// <summary>
+        /// Benützen um Connection zu öffnen
+        /// Wenn NUR diese Methode zum öffnen benützt wird braucht man KEIN Close mehr!
+        /// </summary>
+        static public void Open()
+        {
+            Connection.Close(); //TODO add Exception thingy
+            //TODO Connection.State
+            Connection.Open();
+        }
     }
 }
