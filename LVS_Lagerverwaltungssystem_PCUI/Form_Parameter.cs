a33cbbf150b6715d9c739ec94bbe9b43055aec8a
Chaftalie <chaftalie@Lapritrop>
2019-01-23 10:18:54 +0100

    Changed to 64Bit

--- LVS_Lagerverwaltungssystem_PCUI/Form_Parameter.cs
+++ LVS_Lagerverwaltungssystem_PCUI/Form_Parameter.cs
@@ -23,11 +23,11 @@ namespace LVS_Lagerverwaltungssystem_PCUI
 
         private void Fill_Database_tBx_thingys()
         {
-            tBx_DB_Name.Text =          Properties.Settings.Default.Database_Name;
-            tBx_IP.Text =                Properties.Settings.Default.Database_IP;
-            tBx_Port.Text =                 Properties.Settings.Default.Database_Port;
-            tBx_Login_Name.Text =           Properties.Settings.Default.Database_Login_Name;
-            tBx_Login_Password.Text =   Properties.Settings.Default.Database_Login_Password;
+            tBx_DB_Name.Text = Properties.Settings.Default.Database_Name;
+            tBx_IP.Text = Properties.Settings.Default.Database_IP;
+            tBx_Port.Text = Properties.Settings.Default.Database_Port;
+            tBx_Login_Name.Text = Properties.Settings.Default.Database_Login_Name;
+            tBx_Login_Password.Text = Properties.Settings.Default.Database_Login_Password;
         }
 
         private void btn_save_DB_Click(object sender, EventArgs e)
@@ -58,10 +58,11 @@ namespace LVS_Lagerverwaltungssystem_PCUI
             try
             {
                 SQL_methods.Open();
+                MessageBox.Show("You did it\nit works\nlol", "Congrats", MessageBoxButtons.OK);
             }
-            catch(Exception exception)
+            catch (Exception exception)
             {
-                MessageBox.Show("Something did not work\nSome informatione (I hope it can help):"+ exception.Message,"Warnung",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                MessageBox.Show("Something did not work\nSome informatione (I hope it can help):" + exception.Message, "Warnung", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
