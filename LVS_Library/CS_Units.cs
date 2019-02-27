94e01b3c71c30e90a3261c0af8cbb6c70aa4c948
Chaftalie <chaftalie@Lapritrop>
2019-02-27 10:03:32 +0100

    Changed CS_Unit

--- LVS_Library/CS_Units.cs
+++ LVS_Library/CS_Units.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LVS_Lagerverwaltungssystem_PCUI
+namespace LVS_Library
 {
-    class Units
+    class Unit
     {
         private int id;
         private float si_unit;
         private string description;
         private string name;
+
+        /// <summary>
+        /// Generates a new Unit for usage in a Storage or Item
+        /// </summary>
+        /// <param name="SI Unit"></param>
+        /// <param name="Description"></param>
+        /// <param name="Name"></param>
+        public Unit(float _si_unit, string _description, string _name)
+        {
+            SI_Units = _si_unit;
+            Description = _description;
+            Name = _name;
+        }
+
+
+
 
         public int ID
         {
@@ -25,6 +41,40 @@ namespace LVS_Lagerverwaltungssystem_PCUI
             }
         }
 
-        private float Si
+        public float SI_Units
+        {
+            get
+            {
+                return si_unit;
+            }
+            set
+            {
+                si_unit = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
     }
 }
