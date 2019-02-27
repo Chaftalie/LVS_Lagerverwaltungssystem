d55529a68563c19ca24b24f50c44b51a56310a8f
chaftalie <chaftalie@DESKTOP-IHNFJ9N>
2019-02-27 09:53:41 +0100

    added Storage class

---
+++ LVS_Library/CS_Storage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LVS_Library
+{
+    public class Storage
+    {
+        private float width;
+        private float length;
+        private float height;
+        private string name;
+        private string description;
+        private float element_count; //Stückzahl, länge der items
+        private Units unit;
+        private Storage parent;
+        private int id;
+
+        public float Width { get => width; set => width = value; }
+        public float Length { get => length; set => length = value; }
+        public float Height { get => height; set => height = value; }
+        public string Name { get => name; set => name = value; }
+        public string Description { get => description; set => description = value; }
+        public float Element_count { get => element_count; set => element_count = value; }
+        public Units Unit { get => unit; set => unit = value; }
+        public Storage Parent { get => parent; set => parent = value; }
+        public int Id { get => id; set => id = value; }
+    }
+}
