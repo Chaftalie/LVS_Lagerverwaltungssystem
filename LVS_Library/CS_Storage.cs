dcafef92fad23218b8e64bd885976a214d53f374
chaftalie <chaftalie@DESKTOP-IHNFJ9N>
2019-02-27 09:58:38 +0100

    Added constructor

--- LVS_Library/CS_Storage.cs
+++ LVS_Library/CS_Storage.cs
@@ -14,7 +14,7 @@ namespace LVS_Library
         private string name;
         private string description;
         private float element_count; //Stückzahl, länge der items
-        private Units unit;
+        private Unit unit;
         private Storage parent;
         private int id;
 
@@ -24,8 +24,18 @@ namespace LVS_Library
         public string Name { get => name; set => name = value; }
         public string Description { get => description; set => description = value; }
         public float Element_count { get => element_count; set => element_count = value; }
-        public Units Unit { get => unit; set => unit = value; }
+        public Unit Unit { get => unit; set => unit = value; }
         public Storage Parent { get => parent; set => parent = value; }
         public int Id { get => id; set => id = value; }
+
+        public Storage(float width, float length, float height, string name, Storage parent, int id)
+        {
+            Width = width;
+            Length = length;
+            Height = height;
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Parent = parent;
+            Id = id;
+        }
     }
 }
