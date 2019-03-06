788c09f3665926696d0b9974f78c71cbc5d52b1e
LetsManu <manuel.weissboeck@htlvb.at>
2019-03-06 18:30:54 +0100

    Draq Fenuction added for Form_Login

--- LVS_Lagerverwaltungssystem_PCUI/Form_Login.Designer.cs
+++ LVS_Lagerverwaltungssystem_PCUI/Form_Login.Designer.cs
@@ -36,8 +36,10 @@
             this.txt_login_password = new System.Windows.Forms.TextBox();
             this.btn_login = new System.Windows.Forms.Button();
             this.btn_exit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel3.SuspendLayout();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -49,7 +51,7 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(173, 20);
+            this.label1.Location = new System.Drawing.Point(178, 35);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(69, 33);
             this.label1.TabIndex = 0;
@@ -64,7 +66,7 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.DeepSkyBlue;
-            this.label2.Location = new System.Drawing.Point(232, 20);
+            this.label2.Location = new System.Drawing.Point(237, 35);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(144, 33);
             this.label2.TabIndex = 1;
@@ -90,7 +92,7 @@
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel3.Controls.Add(this.txt_login_name);
             this.panel3.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.panel3.Location = new System.Drawing.Point(116, 68);
+            this.panel3.Location = new System.Drawing.Point(121, 83);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(309, 53);
             this.panel3.TabIndex = 5;
@@ -104,7 +106,7 @@
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.txt_login_password);
             this.panel1.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.panel1.Location = new System.Drawing.Point(116, 120);
+            this.panel1.Location = new System.Drawing.Point(121, 135);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(309, 53);
             this.panel1.TabIndex = 6;
@@ -127,7 +129,7 @@
             this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_login.ForeColor = System.Drawing.Color.White;
-            this.btn_login.Location = new System.Drawing.Point(116, 188);
+            this.btn_login.Location = new System.Drawing.Point(121, 203);
             this.btn_login.Name = "btn_login";
             this.btn_login.Size = new System.Drawing.Size(309, 46);
             this.btn_login.TabIndex = 7;
@@ -137,14 +139,29 @@
             // 
             // btn_exit
             // 
+            this.btn_exit.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btn_exit.FlatAppearance.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.btn_exit.FlatAppearance.BorderSize = 0;
             this.btn_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_exit.Location = new System.Drawing.Point(518, -1);
+            this.btn_exit.Location = new System.Drawing.Point(517, -1);
             this.btn_exit.Name = "btn_exit";
             this.btn_exit.Size = new System.Drawing.Size(32, 23);
             this.btn_exit.TabIndex = 8;
             this.btn_exit.Text = "X";
-            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.UseVisualStyleBackColor = false;
             this.btn_exit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(520, 23);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
             // Form_Login
             // 
@@ -152,6 +169,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSlateGray;
             this.ClientSize = new System.Drawing.Size(549, 279);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btn_exit);
             this.Controls.Add(this.btn_login);
             this.Controls.Add(this.panel1);
@@ -166,6 +184,7 @@
             this.panel3.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,5 +200,6 @@
         private System.Windows.Forms.TextBox txt_login_password;
         private System.Windows.Forms.Button btn_login;
         private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- LVS_Lagerverwaltungssystem_PCUI/Form_Login.cs
+++ LVS_Lagerverwaltungssystem_PCUI/Form_Login.cs
@@ -26,5 +26,32 @@ namespace LVS_Lagerverwaltungssystem_PCUI
         {
 
         }
+
+        private bool mouseDown;
+        private Point lastLocation;
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+        
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            mouseDown = false;
+
+        }
     }
 }
