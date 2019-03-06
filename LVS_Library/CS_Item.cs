9501ed52b0b22dc6046fe3809aa409a915ad465f
chaftalie <chaftalie@DESKTOP-IHNFJ9N>
2019-03-06 10:56:23 +0100

    Added Is_Allocated Methode

--- LVS_Library/CS_Item.cs
+++ LVS_Library/CS_Item.cs
@@ -87,7 +87,7 @@ namespace LVS_Library
         }
 
         public Item(string _name, string _description, float _width, float _length, float _height, Unit _unit, Category _category, List<Property> _properties, string _image, int _id)
-        { 
+        {
             Name = _name;
             Description = _description;
             Width = _width;
@@ -111,13 +111,13 @@ namespace LVS_Library
                 "('{0}', '{1}', {2}, {3}, {4}, {5}, {6}, '{7}')",
                 item.Name, item.Description, item.Unit.ID, item.Category.ID, item.Length, item.Width, item.Height, item.Image));
         }
-         
+
         public static bool Is_active(Item item)
         {
-            string sql = "SELECT active FROM storage_elements WHERE id = " + item.ID ;
+            string sql = "SELECT active FROM storage_elements WHERE id = " + item.ID;
             OdbcCommand cmd = new OdbcCommand(sql, DB.Connection);
             SQL_methods.Open();
-            return (bool)cmd.ExecuteScalar();
+            return ( bool ) cmd.ExecuteScalar();
         }
 
         public static void Deactivate(Item item)
@@ -137,13 +137,13 @@ namespace LVS_Library
                 "AND element_size_l = '{4}' " +
                 "AND element_size_w = '{5}' " +
                 "AND element_size_h = '{6}' " +
-                "AND element_image = '{7}'", 
+                "AND element_image = '{7}'",
                 item.Name, item.Description, item.Unit.ID, item.Category.ID, item.Length, item.Width, item.Height, item.Image);
 
             OdbcCommand cmd = new OdbcCommand(sql, DB.Connection);
             SQL_methods.Open();
             OdbcDataReader sqlReader = cmd.ExecuteReader();
-            return (int)sqlReader[0];
+            return ( int ) sqlReader[0];
         }
 
         public static bool Exists_in_DB(Item item)
@@ -162,8 +162,23 @@ namespace LVS_Library
             OdbcCommand cmd = new OdbcCommand(sql, DB.Connection);
             SQL_methods.Open();
             OdbcDataReader sqlReader = cmd.ExecuteReader();
-            return ((string)sqlReader[0] == "1");
+            return ( ( string ) sqlReader[0] == "1" );
+        }
 
+        public static bool Is_Allocated(Item item)
+        {
+            if (Exists_in_DB(item))
+            {
+                int id = Get_DB_ID(item);
+                string sql = "SELECT Count(*) FROM element_location WHERE element_location.element_id = " + item.ID;
+                OdbcCommand cmd = new OdbcCommand(sql, DB.Connection);
+                SQL_methods.Open();
+                return ( bool ) cmd.ExecuteScalar();
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
