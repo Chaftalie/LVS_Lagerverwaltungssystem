2a353ed15039ea8978527b15b9f0ce2f6b1b5974
chaftalie <chaftalie@DESKTOP-IHNFJ9N>
2019-03-13 11:13:38 +0100

    Added is same protection to category in element

--- LVS_Lagerverwaltungssystem_PCUI/Form_Main.cs
+++ LVS_Lagerverwaltungssystem_PCUI/Form_Main.cs
@@ -34,8 +34,10 @@ namespace LVS_Lagerverwaltungssystem_PCUI
 
         private void btn_elements_cat_add_Click(object sender, EventArgs e)
         {
-            if(lbx_elements_used_cat.Items.Contains(lbx_elements_all_cat.SelectedItem))
-            lbx_elements_used_cat.Items.Add(lbx_elements_all_cat.SelectedItem);
+            if (!lbx_elements_used_cat.Items.Contains(lbx_elements_all_cat.SelectedItem))
+            {
+                lbx_elements_used_cat.Items.Add(lbx_elements_all_cat.SelectedItem);
+            }
         }
 
         private void btn_elements_cat_del_Click(object sender, EventArgs e)
