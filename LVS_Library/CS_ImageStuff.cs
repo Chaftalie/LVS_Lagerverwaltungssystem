2c63f2c7c3dabfcc07242b1bc7eeaa80610bbc94
chaftalie <chaftalie@DESKTOP-IHNFJ9N>
2019-03-27 10:51:30 +0100

    Added image to base64 and back

---
+++ LVS_Library/CS_ImageStuff.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LVS_Library
+{
+    class ImageStuff
+    {
+        public static Image GetImageFromString(string base64String)
+        {
+            byte[ ] buffer = Convert.FromBase64String(base64String);
+
+            if (buffer != null)
+            {
+                ImageConverter ic = new ImageConverter();
+                return ic.ConvertFrom(buffer) as Image;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static string GetStringFromImage(Image image)
+        {
+            if (image != null)
+            {
+                ImageConverter ic = new ImageConverter();
+                byte[ ] buffer = ( byte[ ] ) ic.ConvertTo(image, typeof(byte[ ]));
+                return Convert.ToBase64String(
+                    buffer,
+                    Base64FormattingOptions.InsertLineBreaks);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
