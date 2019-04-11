79d4dbe2d076c5011c559bfa8963c8a3e15b8bc2
chaftalie <chaftalie@DESKTOP-IHNFJ9N>
2019-04-11 15:05:39 +0200

    Added Get_from_ID() Methode

--- LVS_Library/CS_Storage.cs
+++ LVS_Library/CS_Storage.cs
@@ -34,7 +34,7 @@ namespace LVS_Library
         public string Storage_dataID { get => storage_dataID; set => storage_dataID = value; }
         public int Max_count { get => max_count; set => max_count = value; }
 
-        public Storage(float width, float length, float height, string name, string description, Unit unit, Storage parent, string storage_dataID, int max_count)
+        public Storage(float width, float length, float height, string name, string description, Unit unit, int parent_ID, string storage_dataID, int max_count)
         {
             Width = width;
             Length = length;
@@ -105,7 +105,7 @@ namespace LVS_Library
                 "(parent_id,storage_name,storage_description,storage_size_l,storage_size_w,storage_size_h,storage_unit_id,storage_max_elements,storage_dataID) " +
                 "VALUES " +
                 "({0}, '{1}', '{2}', {3}, {4}, {5}, {6}, {7}, '{8}')",
-                storage.Parent.ID, storage.Name, storage.Description, storage.Length, storage.Width, storage.Height, storage.Unit.ID, storage.Max_count, storage.Storage_dataID));
+                storage?.Parent?.ID == null ? "''" : storage.Parent.ID.ToString(), storage.Name, storage.Description, storage.Length, storage.Width, storage.Height, storage.Unit.ID, storage.Max_count, storage.Storage_dataID));
         }
 
         public static bool Is_active(Storage storage)
@@ -126,13 +126,13 @@ namespace LVS_Library
             string sql = string.Format("" +
                 "SELECT COUNT(*) " +
                 "FROM storage_location WHERE " +
-                "element_name = '{0}' " +
-                "AND element_description = '{1}' " +
-                "AND element_unit_id = '{2}' " +
-                "AND element_size_l = '{3}' " +
-                "AND element_size_w = '{4}' " +
-                "AND element_size_h = '{5}' " +
-                "AND element_dataID = '{6}'", storage.Name, storage.Description, storage.Unit.ID, storage.Length, storage.Width, storage.Height, storage.Storage_dataID);
+                "storage_name = '{0}' " +
+                "AND storage_description = '{1}' " +
+                "AND storage_unit_id = '{2}' " +
+                "AND storage_size_l = '{3}' " +
+                "AND storage_size_w = '{4}' " +
+                "AND storage_size_h = '{5}' " +
+                "AND storage_dataID = '{6}'", storage.Name, storage.Description, storage.Unit.ID, storage.Length, storage.Width, storage.Height, storage.Storage_dataID);
             OdbcCommand cmd = new OdbcCommand(sql, DB.Connection);
             SQL_methods.Open();
             OdbcDataReader sqlReader = cmd.ExecuteReader();
@@ -147,7 +147,7 @@ namespace LVS_Library
             foreach (var result in properties)
             {
                 String sql = "INSERT INTO storage_properties (storage_id,property_id) VALUES (" +
-                                storage.ID + "," + result.ID + ")";
+                                storage_DB_ID + "," + result.ID + ")";
                 SQL_methods.SQL_exec(sql);
             }
         }
@@ -157,13 +157,13 @@ namespace LVS_Library
             string sql = string.Format("" +
                             "SELECT ID " +
                             "FROM storage_location WHERE " +
-                            "element_name = '{0}' " +
-                            "AND element_description = '{1}' " +
-                            "AND element_unit_id = '{2}' " +
-                            "AND element_size_l = '{3}' " +
-                            "AND element_size_w = '{4}' " +
-                            "AND element_size_h = '{5}' " +
-                            "AND element_dataID = '{6}'", storage.Name, storage.Description, storage.Unit.ID, storage.Length, storage.Width, storage.Height, storage.Storage_dataID);
+                            "storage_name = '{0}' " +
+                            "AND storage_description = '{1}' " +
+                            "AND storage_unit_id = '{2}' " +
+                            "AND storage_size_l = '{3}' " +
+                            "AND storage_size_w = '{4}' " +
+                            "AND storage_size_h = '{5}' " +
+                            "AND storage_dataID = '{6}'", storage.Name, storage.Description, storage.Unit.ID, storage.Length, storage.Width, storage.Height, storage.Storage_dataID);
             OdbcCommand cmd = new OdbcCommand(sql, DB.Connection);
             SQL_methods.Open();
             OdbcDataReader sqlReader = cmd.ExecuteReader();
@@ -172,5 +172,80 @@ namespace LVS_Library
             return (int)sqlReader[0];
         }
 
+        public static List<Storage> All_Storages()
+        {
+            string sql = "SELECT id FROM storage_location";
+
+            OdbcCommand cmd = new OdbcCommand(sql, DB.Connection);
+            SQL_methods.Open();
+            OdbcDataReader sqlReader = cmd.ExecuteReader();
+
+            List<Storage> storages = new List<Storage>();
+
+            while (sqlReader.Read())
+            {
+                storages.Add(Get_from_ID((int)sqlReader[0]));
+            }
+            return storages;
+        }
+
+        /// <summary>
+        /// Get Storage from DB 
+        /// </summary>
+        /// <param name="db_ID">Primary key of DB</param>
+        /// <returns></returns>
+        public static Storage Get_from_ID(int id)
+        {
+            return Get_from_SQL("SELECT * FROM storage_location WHERE ID = " + id);
+        }
+
+        /// <summary>        
+        /// Get Storage from DB 
+        /// </summary>
+        /// <param name="data_ID">Secondary Key</param>
+        /// <returns></returns>
+        public static Storage Get_from_ID(string data_ID)
+        {
+            return Get_from_SQL("SELECT * FROM storage_location WHERE storage_dataID = " + data_ID);
+        }
+
+        private static Storage Get_from_SQL(string sql)
+        {
+            //can be optimised with an caching function, so the methode caches things like Units or Properties for later use, 
+            //the cache should only live for max 1 second or something like that
+            //another optimisation could be joining sql commands (does not work with cache) at least without additional code
+
+            OdbcCommand cmd = new OdbcCommand(sql, DB.Connection);
+            SQL_methods.Open();
+            OdbcDataReader sqlReader = cmd.ExecuteReader();
+
+            sqlReader.Read();
+
+            #region do that in Unit class (load Unit from ID)
+            //TODO code Methode in Units which is loading one exact unit.
+            sql = "SELECT * FROM units WHERE id =" + (Int64)sqlReader["storage_unit_id"];
+            cmd = new OdbcCommand(sql, DB.Connection);
+            OdbcDataReader sqlReader_unit = cmd.ExecuteReader();
+
+            sqlReader_unit.Read();
+
+            Unit unit = new Unit((string)sqlReader_unit["unit_si"], (string)sqlReader_unit["unit_name"], (string)sqlReader_unit["unit_description"], (int)sqlReader_unit["id"]);
+            #endregion
+
+
+            Storage storage = new Storage(
+                (float)sqlReader["storage_size_w"],
+                (float)sqlReader["storage_size_l"],
+                (float)sqlReader["storage_size_h"],
+                (string)sqlReader["storage_name"],
+                (string)sqlReader["storage_description"],
+                unit,
+                (int)sqlReader["parent_id"],
+                (string)sqlReader["storage_dataID"],
+                (int)sqlReader["storage_max_elements"]
+                );
+
+            return storage;
+        }
     }
 }
